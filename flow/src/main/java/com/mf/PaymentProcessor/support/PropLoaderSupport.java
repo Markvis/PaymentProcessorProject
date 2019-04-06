ceddbd3e6a3e9a3b5b1d4c6ed4939ac4fa869ee0
Mark Favis <favismark@gmail.com>
2019-04-05 17:30:54 -0700

    cleanup

--- flow/src/main/java/com/mf/PaymentProcessor/support/PropLoaderSupport.java
+++ flow/src/main/java/com/mf/PaymentProcessor/support/PropLoaderSupport.java
@@ -1,6 +1,6 @@
 package com.mf.PaymentProcessor.support;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -12,16 +12,10 @@ public class PropLoaderSupport {
 
 		try {
 			Properties prop = new Properties();
-			String propFileName = "application.properties";
-
-			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-			if (inputStream != null) {
-				prop.load(inputStream);
-			} else {
-				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-			}
-
+			String propFileName = System.getProperty("config.file");
+			inputStream = new FileInputStream(propFileName);
+			prop.load(inputStream);
+			
 			System.out.println(prop.getProperty(key));
 			return prop.getProperty(key);
 		} catch (Exception e) {
